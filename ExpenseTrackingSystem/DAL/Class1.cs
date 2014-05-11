c8ce2870e564690013e2951d64710708fed08171
da451 <da451@mail.ru>
2014-05-11 14:14:41 +0300

    + replace all input control on devexpress controls

--- ExpenseTrackingSystem/DAL/Class1.cs
+++ ExpenseTrackingSystem/DAL/Class1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using DAL.Repository.Imp;
 
 namespace DAL
 {
@@ -6,27 +8,7 @@ namespace DAL
     {
         public static void Main(string[] args)
         {
-            //string str =
-            //    @"Data Source = C:\Documents and Settings\Jim\Мои документы\Visual Studio 2005\Projects\TEST\ExpenseTrackingSystem\ExpenseTrackingSystem\MyDB.sdf";
-            //FNHHelper.CreateInstance(str);
-
-            //UnitOfWork uow = new UnitOfWork();
-            //uow.BeginTransaction();
-            //RepositoryUser r = new RepositoryUser(uow);
-            //var fl = r.LogIn("da451", "451452");
-            //try
-            //{
-            //    r.Registration("da451", "da451", "451451");
-
-            //    uow.Commit();
-
-            //}
-            //catch (Exception ex)
-            //{
-            //    uow.RollBack();
-            //}
-
-
+            
         }
     }
 }
